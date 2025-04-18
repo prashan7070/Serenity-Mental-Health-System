571f7b8cecd5119e19a8b5b496b69da748035202
prashan <prashan7070@gmail.com>
2025-04-18 22:34:56 +0530

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module lk.ijse.gdse.serenity_mental_health_system {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens lk.ijse.gdse.serenity_mental_health_system to javafx.fxml;
+    exports lk.ijse.gdse.serenity_mental_health_system;
+}
